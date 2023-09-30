207a140d2ffe04648ca73ffa396e79bfb02a04df
scarf <greenscarf005@gmail.com>
2023-09-30 21:07:14 +0900

    feat: currency conversion

--- utils/units.ts
+++ utils/units.ts
@@ -30,3 +30,22 @@ export const fromLegacyVolume = (x: number): Volume => {
     .with(0, () => `${ml / 1000} L` as const)
     .otherwise(() => `${ml} ml` as const)
 }
+
+export const currency = z.custom<`${number} ${CurrencyUnits}`>((x) => /^\d+ (cent|USD|kUSD)$/.test(x as string))
+export type CurrencyUnits = "cent" | "USD" | "kUSD"
+export type Currency = z.infer<typeof currency>
+
+const usd = 100
+const kusd = 1_000 * usd
+
+/**
+ * converts legacy cent to new currency format.
+ * @param c legacy cent (`1 unit` = `1 cent`)
+ * @return cent or USD or kUSD
+ */
+export const fromLegacyCurrency = (c: number): Currency =>
+  match(c)
+    .when((c) => c % kusd === 0, () => `${c / kusd} kUSD` as const)
+    .when((c) => c % usd === 0, () => `${c / usd} USD` as const)
+    .otherwise(() => `${c} cent` as const)
+
